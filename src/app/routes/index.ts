5be49a48142022fba00d409930ab03bcee713f51
GGTuran <tayyabturan67@gmail.com>
2024-07-10 20:13:17 +0600

    category module completed

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { ProductRoutes } from "../modules/product/productRoutes";
+import { CategoryRoutes } from "../modules/category/categoryRoutes";
 
 
 const router = Router();
@@ -8,6 +9,10 @@ const moduleRoutes = [
     {
         path: '/products',
         route: ProductRoutes,
+    },
+    {
+        path: 'category',
+        route: CategoryRoutes,
     },
 
 ];
